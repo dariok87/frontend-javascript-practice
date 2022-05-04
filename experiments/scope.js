f16bf62923d3801a54ebd8ad7ed0fcbed7ebb787
dariok87 <dkemenovic@gmail.com>
2022-05-04 10:25:21 +0200

    Solve scope task

--- experiments/scope.js
+++ experiments/scope.js
@@ -2,29 +2,29 @@ const A = "A";
 let F;
 
 function doStuff(B) {
-  console.log(B);
+  console.log(B); // working, B parameter is still in scope
   const C = "C";
   let H = "H";
   if (1 + 1 === 2) {
     const D = "D";
     H = "something else";
   }
-  console.log(D);
-  console.log(H);
+  console.log(D); // not working, D was declared in that if statement block
+  console.log(H); // working, H was declared outside the if statement
   F = "F";
 }
 
 let E = 0;
 while (E < 3) {
   E++;
-  console.log(A);
+  console.log(A); // working, the outter block (called the global scope) is still in scope
   const G = "G";
 }
-console.log(E);
-console.log(G);
+console.log(E); // working, E was declared outside the while loop
+console.log(G); // not working, declared inside the while loop and it's over
 
 doStuff("B");
-console.log(B);
-console.log(C);
-console.log(F);
+console.log(B); // not working, the B parameter expires after the function call
+console.log(C); // not working, C was declared inside the function and the function is over
+console.log(F); // working, F was declared in the global scope
 
